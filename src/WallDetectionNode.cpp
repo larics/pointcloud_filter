0d8a787160e8921e620001b7404af67ede96b037
Lovro Markovic <lovro.markovic@fer.hr>
2020-04-21 12:24:15 +0200

    Remove WallDetection functionality from pointcloud_filter

--- src/WallDetectionNode.cpp
+++
@@ -1,9 +0,0 @@
-#include "WallDetection.h"
-
-int main(int argc, char **argv)
-{
-  ros::init(argc, argv, "wall_detection");
-  ros::NodeHandle nh;
-  auto wallDec = std::make_shared<detection::WallDetection>(nh);
-  ros::spin();
-}
